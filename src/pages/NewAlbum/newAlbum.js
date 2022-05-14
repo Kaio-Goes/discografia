78ac911b8498d2900d84ff90587586fc88186443
Kaio-Goes <kaiogoes1999@gmail.com>
2022-05-14 18:03:35 -0300

    adicionando validações aos campos newAlbum

--- src/pages/NewAlbum/newAlbum.js
+++ src/pages/NewAlbum/newAlbum.js
@@ -2,10 +2,24 @@ import React from 'react'
 import '../../../src/styles.css'
 import Card from '@mui/material/Card';
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup"
+
+const validationPost = yup.object().shape({
+    name: yup.string().required("O nome do Albúm é obrigatório")
+            .min(1, 'O mínimo do nome do Albúm é de 1 caractere')
+            .max(30, 'O Albúm precisa ter menos de 30 caracteres'),
+    year:  yup.number()
+            .typeError('O ano do Albúm é obrigatório')
+            .min(1900, 'Ano mínimo é 1900')
+            .max(2022, 'Ano máximo é 2022'),
+})
 
 function NewAlbum(){
 
-    const { register, handleSubmit, formState: { erros }} = useForm();
+    const { register, handleSubmit, formState: { errors }} = useForm({
+        resolver: yupResolver(validationPost)
+    });
 
     const addPost = data => console.log(data)
 
@@ -28,10 +42,15 @@ function NewAlbum(){
                                 <div className="fields">
                                     <label>Nome do Albúm:</label>
                                     <input type="text" title="name" {...register("name")}/>
+                                    <p className="error-message">{errors.name?.message}</p>
                                 </div>
                                 <div className="fields">
                                     <label>Ano do Albúm:</label>
-                                    <input type="number" title="year" {...register("year")}/>
+                                    <input type="number" title="year" {...register("year",{ pattern: {
+        value: /^[0-9]+$/,
+        message: 'Please enter a number',
+    },})}/>
+                                    <p className="error-message">{errors.year?.message}</p>
                                 </div>
                                 <div className="btn-post">
                                     <button type="submit">Enviar</button>
