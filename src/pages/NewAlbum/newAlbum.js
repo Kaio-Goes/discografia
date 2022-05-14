40199bb421d9a8fa6062271754d6323a7058cb8b
Kaio-Goes <kaiogoes1999@gmail.com>
2022-05-14 02:33:37 -0300

    criando a base da tela

--- src/pages/NewAlbum/newAlbum.js
+++ src/pages/NewAlbum/newAlbum.js
@@ -5,7 +5,16 @@ import Card from '@mui/material/Card';
 function newAlbum(){
     return(
         <div className="container">
-                <p>oiii</p>
+            <Card sx={{width:650 , height: 100, marginTop: -10}}>
+                <br/>
+                <img className='image' src={require('../../img/logo.png')}/>
+                <p className='discografia'>Discografia</p>
+            </Card>
+            <br/>
+            <Card  sx={{width:650 , backgroundColor:  'rgba(255,255,255,0.6)'}}>
+                <br/>
+                <a className="a" href="http://localhost:3000/">Voltar</a>
+            </Card>
         </div>
     )
 }
